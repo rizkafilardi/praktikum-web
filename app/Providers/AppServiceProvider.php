fc0b9914cfedece8f203422d6a73ab2cab268fee
rizkafilardi <153422810+rizkafilardi@users.noreply.github.com>
2025-05-22 15:28:30 +0800

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 use Illuminate\Support\ServiceProvider;
@@ -14,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     }
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Gate::define("view-student", function (User $user) {
             if ($user->role === "admin" || $user->role === "guest") {
                 return true;
